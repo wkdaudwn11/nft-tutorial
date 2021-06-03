d9afb7304687da53e800d2e2868c54e4cc892699
JangMyungJoo <wkdaudwn11@naver.com>
2021-06-03 11:41:16 +0900

    Truffle Comfile 후 Migrate까지 해보기. test 코드도 간단하게 작성

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -3,18 +3,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // import "./ERC721.sol";
-// pragma solidity >=0.5.16;
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 // ERC721.sol 파일 생성 명령어
 // ./node_modules/.bin/truffle-flattener ./node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol > src/contracts/ERC721.sol
 
 contract Color is ERC721 { 
-
     // ERC721한테 name, symbol을 파라미터 값으로 줌
-    // constructor() ERC721("Color", "COLOR") public {
-    // }
-
     constructor() ERC721("Color", "COLOR") public {
     }
-
 }
---
+++ test/Color.test.js
@@ -0,0 +1,15 @@
+const { FormControlStatic } = require('react-bootstrap');
+
+const Color = artifacts.require('./Color.sol');
+
+require('chai').use(require('chai-as-promised')).should()
+
+contract('Color', (accounts) => {
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            
+        })
+    })
+
+})
