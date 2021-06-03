e2f451058d1575d14c03039caa257e537ab10a3f
JangMyungJoo <wkdaudwn11@naver.com>
2021-06-03 14:34:35 +0900

    Test 코드 작성 완료

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -2,13 +2,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// import "./ERC721.sol";
-
-// ERC721.sol 파일 생성 명령어
-// ./node_modules/.bin/truffle-flattener ./node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol > src/contracts/ERC721.sol
-
 contract Color is ERC721 { 
-    // ERC721한테 name, symbol을 파라미터 값으로 줌
-    constructor() ERC721("Color", "COLOR") public {
-    }
+    // constructor는 스마트 컨트랙트가 배포 될 때마다 실행되는 함수 (기본적인 생성자 함수 기능과 같은듯)
+    constructor() ERC721("Color", "COLOR") {}
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -1,15 +1,37 @@
-const { FormControlStatic } = require('react-bootstrap');
-
 const Color = artifacts.require('./Color.sol');
 
 require('chai').use(require('chai-as-promised')).should()
 
 contract('Color', (accounts) => {
+    let contract;
+    const name = "Color", symbol = "COLOR";
 
-    describe('deployment', async () => {
-        it('deploys successfully', async () => {
-            
-        })
+    before(async () => {
+        contract = await Color.deployed(); // 배포 결과를 contract에다가 담기?
     })
 
+    describe('deployment', async () => {
+        it('배포 성공', async () => { 
+            const address = contract.address; // 주소 담기
+
+            // 유효한 주소값인지 체크 (이것들 한줄 한줄이 체크하는 것임. false를 리턴하면 테스트 실패)
+            assert.notEqual(address, 0x0) // 비어있는 주소인지 체크 
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+
+            // 주소가 'test'와 일치하는지? => 일치하지 않으므로 테스트 실패하고 에러뜸.
+            // assert.equal(address, "test")
+        })
+
+        it('이름 유효성 검사', async () => {
+            const contractName = await contract.name();
+            assert.equal(contractName, name);
+        })
+
+        it('심볼 유효성 검사', async () => {
+            const contractSymbol = await contract.symbol();
+            assert.equal(contractSymbol, symbol);
+        })
+    })
 })
