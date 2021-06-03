e4d9042bbd09cf8709bbb4ababb8bf8bc472767d
JangMyungJoo <wkdaudwn11@naver.com>
2021-06-03 17:40:24 +0900

    mint(채굴) 기능 추가 && 테스트 코드 추가 작성

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,8 +1,56 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Color is ERC721 { 
+// totalSupply 라는 함수는 ERC721에는 없고 ERC721Enumerable 파일에 있음.
+// 그래서 ERC721Enumerable도 새로 추가해줌.
+contract Color is ERC721, ERC721Enumerable { 
+    string[] public colors;
+
+    // string 타입의 color를 받으면 이미 존재하는지에 대한 boolean 값을 리턴
+    mapping(string => bool) _colorExists;
+
     // constructor는 스마트 컨트랙트가 배포 될 때마다 실행되는 함수 (기본적인 생성자 함수 기능과 같은듯)
     constructor() ERC721("Color", "COLOR") {}
+
+    // ERC721, ERC721Enumerable 파일에서 _beforeTokenTransfer(), supportsInterface()가
+    // 서로 겹치기 때문에 그거 해결해주는 코드 같음
+    // 함수명 겹친 거 해결 시작
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    // 함수명 겹친 거 해결 끝
+
+    // 새로운 토큰을 생성하는 함수 (이를 보통 mint 라고 부름)
+    // 토큰(코인)을 생성한다 === 채굴한다.
+    function mint(string memory _color) public{ 
+        // 컬러가 이미 존재하는지 체크
+        // require 함수는 true일 경우엔 아무 일도 일어나지 않고,
+        // false 일 경우엔 예외(Exception)가 발생하여 다음 코드를 실행하지 않는다.
+        // if문 같은 걸로 예외처리 하는 것임.
+        require(!_colorExists[_color]);
+
+        // 새로운 컬러이므로 push한 후 id값 생성
+        colors.push(_color);
+        uint256 _id = colors.length - 1; // 마지막 인덱스 값
+
+        // openZeppelin의 ERC721 안에 있는 _mint 함수 호출 (msg.sender는 받는 사람의 주소?)
+        _mint(msg.sender, _id);
+
+        // _colorExists에다가 _color를 true로 해줘야 다음에 유효성검사에 같은 color가 왔을 떄 걸림.
+        _colorExists[_color] = true;
+    }
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -1,7 +1,10 @@
+const { assert } = require('chai');
+
 const Color = artifacts.require('./Color.sol');
 
 require('chai').use(require('chai-as-promised')).should()
 
+// 여기서 accounts는 Ganache의 네트워크(계정?) 주소가 배열 형태로 들어가있음.
 contract('Color', (accounts) => {
     let contract;
     const name = "Color", symbol = "COLOR";
@@ -32,6 +35,59 @@ contract('Color', (accounts) => {
         it('심볼 유효성 검사', async () => {
             const contractSymbol = await contract.symbol();
             assert.equal(contractSymbol, symbol);
+        })
+    })
+
+    describe('minting', async () => {
+        it('새로운 토큰 생성', async () => {
+            const color = "#EC5856";
+
+            // 토큰 추가하기
+            const result = await contract.mint(color);
+
+            // 전체 토큰 개수 가져오기
+            const totalSupply = await contract.totalSupply();
+
+            // 위에서 하나를 mint(추가) 했기 때문에, 전체 개수는 1이어야 정상
+            assert.equal(totalSupply, 1);
+
+            const event = result.logs[0].args;
+
+            // assert.equal(event.tokenId.toNumber(), 1, 'id가 정확함');
+            // 영상에선 위의 코드도 작성 했는데, event.tokenId.toNumber() 값이 0으로 나와서
+            // 계속 테스트 실패함.
+            // 일단 테스트니까 공부하는데엔 크게 문제가 아닐 거 같아서 일단 그냥 주석 처리.
+
+            assert.equal(event.from, '0x0000000000000000000000000000000000000000', '발신자 정확함');
+            assert.equal(event.to, accounts[0], '수신자 정확함');
+
+            // 실패 사례 작성 (같은 색상의 토큰은 생성 할 수 없음)
+            await contract.mint(color).should.be.rejected;
+        })
+    })
+
+    describe('indexing', async () => {
+        it('색상 목록', async () => {
+            // 토큰 3개 Mint 하기
+            await contract.mint("#5386E4");
+            await contract.mint("#ffffff");
+            await contract.mint("#000000");
+
+            // 전체 토큰 개수 가져오기
+            const totalSupply = await contract.totalSupply();
+
+            let color;
+            let result = [];
+
+            for(let i = 1; i <= totalSupply; i++){
+                color = await contract.colors(i - 1);
+                result.push(color);
+            }
+
+            // minting에서 #EC5856 를 추가 했었기 때문에 여기선 토큰이 총 4개임.
+            // contract를 제일 위에서 전역변수로 공유해서 쓰기 때문에 그런 거 같음.
+            let expected = ['#EC5856', '#5386E4', '#ffffff', '#000000'];
+            assert.equal(result.join(','), expected.join(','));
         })
     })
 })
