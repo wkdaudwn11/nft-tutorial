dacc3e4f90d442386973ec02b993c17ca173f0eb
JangMyungJoo <wkdaudwn11@naver.com>
2021-06-03 10:50:29 +0900

    컴파일 테스트 성공 Truffle v5.3.8 (core: 5.3.8) Solidity - 0.8.0 (solc-js) Node v16.2.0 Web3.js v1.3.6

--- truffle-config.js
+++ truffle-config.js
@@ -6,13 +6,14 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version: "0.8.0",
       optimizer: {
         enabled: true,
         runs: 200
